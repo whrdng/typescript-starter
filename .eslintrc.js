97a4f1a14110fa937c4b310dd63d7ba6f4435265
Warren Harding <whrdng@gmail.com>
2020-05-22 20:20:17 -0500

    chore: replace eslint config json file with js file.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  }
+}
